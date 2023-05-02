ef1d1e2e0279c9a7df26ba515543ea7d37ca97d3
ibukun Ekujumi <ibukun.ekujumi@gmail.com>
2023-05-02 21:53:54 +0100

    card feature done

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Nav from '../../Components/Nav/Nav'
 import Footer from '../../Components/Footer/Footer'
 import '../../styles/styles.css'
+import Card from '../../Components/Card'
 const Home = () => {
   return (
     <div>
         <Nav />
         <h1 className='height text-center'>Welcome to home Page</h1>
+       <Card />
         <Footer />
     </div>
   )
